6ce3f449db28605dada1e0b8e32b84cdafbbd230
Sam Povilus <kernel.development@povil.us>
2019-11-13 21:34:47 -0700

    working on modifying to work with noise image

--- src/main.cpp
+++ src/main.cpp
@@ -25,6 +25,8 @@ int main( int argc, char** argv )
     }
     Mat image;
     image = imread( samples::findFile( imageName ), IMREAD_COLOR ); // Read the file
+    Mat m1 = Mat(image.size[0],image.size[1], CV_8UC3, Scalar(255,255,0));
+
     if( image.empty() )                      // Check for invalid input
     {
         cout <<  "Could not open or find the image" << std::endl ;
@@ -35,15 +37,15 @@ int main( int argc, char** argv )
     Rect boundingRect(c.center.x-c.radius, c.center.y-c.radius, c.radius*2+1,c.radius*2+1);
 
     // obtain the image ROI:
-    Mat circleROI(image, boundingRect);
+    Mat circleROI(m1, boundingRect);
 
     //draw the circle
     circle(circleROI, Point(c.radius, c.radius), c.radius, Scalar::all(1), -1);
 
 
-    cvtColor(image,image,COLOR_BGR2GRAY);
+    //cvtColor(image,image,COLOR_BGR2GRAY);
     namedWindow( "Display window", WINDOW_AUTOSIZE ); // Create a window for display.
-    imshow( "Display window", image );                // Show our image inside it.
+    imshow( "Display window", m1 );                // Show our image inside it.
     waitKey(0); // Wait for a keystroke in the window
     return 0;
 }
