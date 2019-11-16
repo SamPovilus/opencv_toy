a38beb6e8ccf4977e24dbe84c39d12bf6544603e
Sam Povilus <kernel.development@povil.us>
2019-11-16 12:33:44 -0700

    trying to add target

--- src/main.cpp
+++ src/main.cpp
@@ -33,19 +33,45 @@ int main( int argc, char** argv )
         return -1;
     }
 
+    int targetSize = 5;
+    cv::Mat gaussX = cv::getGaussianKernel(targetSize, -1, CV_32F);
+    cv::Mat gaussY = cv::getGaussianKernel(targetSize, -1, CV_32F);
+    cv::Mat gauss2d = gaussX * gaussY.t();
+    gauss2d *= 200;
+
+    cout << "gauss2d = "<< endl << " "  << gauss2d << endl << endl;
+    
     // get the circle's bounding rect
     Rect boundingRect(c.center.x-c.radius, c.center.y-c.radius, c.radius*2+1,c.radius*2+1);
 
+    int xTargetLoc = c.center.x;
+    int yTargetLoc = c.center.y;
+
+    Mat target = image.colRange(xTargetLoc,xTargetLoc+targetSize).rowRange(yTargetLoc,yTargetLoc+targetSize);
+
+    cout << "image 1= " << endl << image.colRange(xTargetLoc,xTargetLoc+targetSize).rowRange(yTargetLoc,yTargetLoc+targetSize) << endl << endl;
+
+    cout << "image corner = " << endl << image.colRange(0,3).rowRange(0,3) << endl << endl;
+
     // obtain the image ROI:
-    Mat circleROI(m1, boundingRect);
+    Mat circleROI(image, boundingRect);
+
+    cout << "image 2= " << endl << image.colRange(xTargetLoc,xTargetLoc+targetSize).rowRange(yTargetLoc,yTargetLoc+targetSize) << endl << endl;
 
     //draw the circle
     circle(circleROI, Point(c.radius, c.radius), c.radius, Scalar::all(1), -1);
 
+    cout << "image 4= " << endl << image.colRange(xTargetLoc,xTargetLoc+targetSize).rowRange(yTargetLoc,yTargetLoc+targetSize) << endl << endl;
 
-    //cvtColor(image,image,COLOR_BGR2GRAY);
+
+    gauss2d.copyTo(target);
+
+    cout << "image 5= " << endl << image.colRange(xTargetLoc,xTargetLoc+targetSize).rowRange(yTargetLoc,yTargetLoc+targetSize) << endl << endl;
+
+
+    cvtColor(image,image,COLOR_BGR2GRAY);
     namedWindow( "Display window", WINDOW_AUTOSIZE ); // Create a window for display.
-    imshow( "Display window", m1 );                // Show our image inside it.
+    imshow( "Display window", image );                // Show our image inside it.
     waitKey(0); // Wait for a keystroke in the window
     return 0;
 }
