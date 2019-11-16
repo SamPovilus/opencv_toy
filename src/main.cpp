d4d65f8f1bb385adc13b9f3bae87ea0fe0e9bf0a
Sam Povilus <kernel.development@povil.us>
2019-11-16 16:23:19 -0700

    got gaussian working

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,7 @@ typedef struct {
 int main( int argc, char** argv )
 {
     Circle c;
-    c.radius = 100.0;
+    c.radius = 40;
     c.center.x = 100;
     c.center.y = 150;
     String imageName( "HappyFish.jpg" ); // by default
@@ -33,11 +33,11 @@ int main( int argc, char** argv )
         return -1;
     }
 
-    int targetSize = 5;
+    int targetSize = 50;
     cv::Mat gaussX = cv::getGaussianKernel(targetSize, -1, CV_32F);
     cv::Mat gaussY = cv::getGaussianKernel(targetSize, -1, CV_32F);
     cv::Mat gauss2d = gaussX * gaussY.t();
-    gauss2d *= 200;
+    gauss2d *= 5000;
 
     cout << "gauss2d = "<< endl << " "  << gauss2d << endl << endl;
     
@@ -46,30 +46,34 @@ int main( int argc, char** argv )
 
     int xTargetLoc = c.center.x;
     int yTargetLoc = c.center.y;
+        cvtColor(image,image,COLOR_BGR2GRAY);
 
-    Mat target = image.colRange(xTargetLoc,xTargetLoc+targetSize).rowRange(yTargetLoc,yTargetLoc+targetSize);
+    gauss2d.copyTo(image(Rect(xTargetLoc, yTargetLoc, targetSize, targetSize)));
+
+    //Mat target = image.colRange(xTargetLoc,xTargetLoc+targetSize).rowRange(yTargetLoc,yTargetLoc+targetSize);
 
     cout << "image 1= " << endl << image.colRange(xTargetLoc,xTargetLoc+targetSize).rowRange(yTargetLoc,yTargetLoc+targetSize) << endl << endl;
 
     cout << "image corner = " << endl << image.colRange(0,3).rowRange(0,3) << endl << endl;
 
     // obtain the image ROI:
-    Mat circleROI(image, boundingRect);
+    //Mat circleROI(image, boundingRect);
 
     cout << "image 2= " << endl << image.colRange(xTargetLoc,xTargetLoc+targetSize).rowRange(yTargetLoc,yTargetLoc+targetSize) << endl << endl;
+    //cout << "circleROI 1= " << endl << circleROI << endl << endl;
 
     //draw the circle
-    circle(circleROI, Point(c.radius, c.radius), c.radius, Scalar::all(1), -1);
+    //circle(circleROI, Point(c.radius, c.radius), c.radius, Scalar::all(1), -1);
+    //cout << "circleROI = " << endl << circleROI << endl << endl;
 
     cout << "image 4= " << endl << image.colRange(xTargetLoc,xTargetLoc+targetSize).rowRange(yTargetLoc,yTargetLoc+targetSize) << endl << endl;
 
-
-    gauss2d.copyTo(target);
+    //gauss2d.copyTo(target );
 
     cout << "image 5= " << endl << image.colRange(xTargetLoc,xTargetLoc+targetSize).rowRange(yTargetLoc,yTargetLoc+targetSize) << endl << endl;
 
 
-    cvtColor(image,image,COLOR_BGR2GRAY);
+    //cvtColor(image,image,COLOR_BGR2GRAY);
     namedWindow( "Display window", WINDOW_AUTOSIZE ); // Create a window for display.
     imshow( "Display window", image );                // Show our image inside it.
     waitKey(0); // Wait for a keystroke in the window
